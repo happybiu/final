f3b40de257dd4207d0bd66cb379048eabae2bdcf
happylocalgit <22021333@zju.edu.cn>
2021-03-04 21:18:39 +0800

    add lfw_pairs.py

---
+++ lfw_pairs.py
@@ -0,0 +1,142 @@
+import face_recognition
+import cv2
+import numpy as np
+
+# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
+# other example, but it includes some basic performance tweaks to make things run a lot faster:
+#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
+#   2. Only detect faces in every other frame of video.
+
+# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+
+# Get a reference to webcam #0 (the default one)
+# video_capture = cv2.VideoCapture(0)
+
+# # Load a sample picture and learn how to recognize it.
+# obama_image = face_recognition.load_image_file("test1.jpg")
+# obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+# # Load a second sample picture and learn how to recognize it.
+# biden_image = face_recognition.load_image_file("test2.jpg")
+# biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+lfw_path = 'LFW/lfw'
+with open('./LFW/pairs.txt') as f: 
+    next(f)
+    for line in f:
+        line_list = line.strip('\n').split('\t')
+        if len(line_list) == 3:
+            img1_path = lfw_path + '/' + line_list[0] + '/' + line_list[0] + '_' + line_list[1].rjust(4, '0') + '.jpg'
+            img2_path = lfw_path + '/' + line_list[0] + '/' + line_list[0] + '_' + line_list[2].rjust(4, '0') + '.jpg'
+        elif len(line_list) == 4:
+            img1_path = lfw_path + '/' + line_list[0] + '/' + line_list[0] + '_' + line_list[1].rjust(4, '0') + '.jpg'
+            img2_path = lfw_path + '/' + line_list[2] + '/' + line_list[2] + '_' + line_list[3].rjust(4, '0') + '.jpg'
+
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [
+    obama_face_encoding,
+    biden_face_encoding
+]
+known_face_names = [
+    "test1",
+    "test2"
+]
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+while True:
+    # # 识别到人脸就停止，等到输入键再开始
+    # stop = False
+
+    # # Grab a single frame of video
+    # ret, frame = video_capture.read()
+    img1 = cv2.imread(img1_path)
+    img2 = cv2.imread(img2_path)
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_img1 = cv2.resize(img1, (0, 0), fx=0.25, fy=0.25)
+    small_img2 = cv2.resize(img2, (0, 0), fx=0.25, fy=0.25)
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_img1 = small_img1[:, :, ::-1]
+    rgb_small_img2 = small_img2[:, :, ::-1]
+    # Only process every other frame of video to save time
+    # if process_this_frame:
+    # Find all the faces and face encodings in the current frame of video
+    face_locations1 = face_recognition.face_locations(rgb_small_img1)
+    face_encodings1 = face_recognition.face_encodings(rgb_small_img1, face_locations)
+    face_locations2 = face_recognition.face_locations(rgb_small_img2)
+    face_encodings2 = face_recognition.face_encodings(rgb_small_img2, face_locations)
+
+    face_distances = face_recognition.face_distance(face_encodings1, face_encoding2)
+
+    
+
+
+
+
+    
+    face_names = []
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
+
+        # # If a match was found in known_face_encodings, just use the first one.
+        # if True in matches:
+        #     first_match_index = matches.index(True)
+        #     name = known_face_names[first_match_index]
+
+        # Or instead, use the known face with the smallest distance to the new face
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+
+        face_names.append(name)
+
+    # process_this_frame = not process_this_frame
+
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        # 显示最像的图片在左上角
+        if name != 'unknown':
+            stop = True
+            best_img = cv2.imread(name + ".jpg")
+            best_img = cv2.resize(best_img, (100, 100))
+            frame[0:100, 0:100] = best_img
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # 识别到人脸就停止，等到输入键再开始
+    if stop:
+        cv2.waitKey(0)
+        stop = False
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
